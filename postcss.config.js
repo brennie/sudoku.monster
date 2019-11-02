c68af45dcb57ff69a0ba74180fdca6bbf401e335
Barret Rennie <barret@brennie.ca>
2019-11-02 18:18:38 -0400

    Define colours with postcss-simple-vars
    We now handle imports via postcss-import and are no longer using
    autoprefixer, since postcss-preset-env can cover its use cases.

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,17 @@
 "use strict";
 
+const path = require("path");
+
+const srcPath = path.resolve(__dirname, "src");
 const isProduction = process.env.NODE_ENV === "production";
 
 const plugins = [
-  require("autoprefixer"),
+  require("postcss-import")({
+    path: srcPath,
+  }),
   require("postcss-preset-env"),
   require("postcss-nested"),
+  require("postcss-simple-vars"),
 ];
 
 if (isProduction) {
